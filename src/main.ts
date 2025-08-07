674dad12676424a551db19204510ae1f47928bc9
slogup-woodrow <woodrow@slogup.com>
2025-08-08 08:30:43 +0900

    feat:swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConstants } from './shared/constants/swagger.constants';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { commonConstants } from './shared/constants/common.constants';
+import basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  /**
+   * Swagger
+   */
+  if (process.env.NODE_ENV !== commonConstants.props.nodeEnvs.LOCAL) {
+    // local 외의 환경변수에서는 Basic authentication 설정
+    app.use(
+      [swaggerConstants.props.SWAGGER_PATH],
+      basicAuth({
+        challenge: true,
+        users: {
+          [swaggerConstants.props.SWAGGER_USER]:
+            swaggerConstants.props.SWAGGER_PASSWORD,
+        },
+      }),
+    );
+  }
+
+  const config = new DocumentBuilder()
+    .setTitle(swaggerConstants.props.SWAGGER_TITLE)
+    .setDescription(swaggerConstants.props.SWAGGER_DESCRIPTION)
+    .setVersion(swaggerConstants.props.SWAGGER_VERSION)
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'jwt' },
+      swaggerConstants.auth.BEARER_TOKEN,
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(swaggerConstants.props.SWAGGER_PATH, app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // 웹(swagger)에서 새로고침을 해도 authorization(token) 유지
+      // docExpansion: 'none', // 모든 태그가 닫힌 상태로 swagger 페이지 오픈
+      tagsSorter: 'alpha', // 태그를 정렬합니다.
+    },
+  });
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
